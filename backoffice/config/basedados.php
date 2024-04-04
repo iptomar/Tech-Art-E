b806bc35d98a2a332d35ad534751fcd4a7c7c304
Gonçalo Costa <gfmcosta03@gmail.com>
2024-04-04 18:38:33 +0100

    changed config values o db. Created form to select projects to newsletter

--- backoffice/config/basedados.php
+++ backoffice/config/basedados.php
@@ -1,10 +1,16 @@
 <?php
 require 'credentials.php';
 
-$servername = "127.0.0.1";
-$username = USERNAME;
-$password = PASSWORD;
-$dbname = "technart";
+// $servername = "127.0.0.1";
+// $username = USERNAME;
+// $password = PASSWORD;
+// $dbname = "technart";
+// $charset = "utf8mb4";
+
+$servername = "94.46.180.24";
+$username = 'costa';
+$password = '12345';
+$dbname = "Tech-Art";
 $charset = "utf8mb4";
 
 // Create connection
