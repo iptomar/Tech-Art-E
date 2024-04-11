0166c563685cbab87cd08889f68365fbde61e1bc
Tiago Oliveira <86232762+tiago0liveira77@users.noreply.github.com>
2024-04-11 21:06:07 +0100

    Changed logic to use json and dynamic email receivers

--- backoffice/newsletter/newsSelectForm.php
+++ backoffice/newsletter/newsSelectForm.php
@@ -1,27 +1,37 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Projetos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <style>
         .modal-dialog {
-            max-width: 800px; /* Defina a largura máxima do modal */
-            margin: auto; /* Centralize o modal horizontalmente */
-            margin-top: calc(30vh - 180px); /* Calcula a margem superior para centralizar verticalmente */
+            max-width: 70%;
+            /* Defina a largura máxima do modal */
+            margin: auto;
+            /* Centralize o modal horizontalmente */
+            margin-top: calc(30vh - 210px);
+            /* Calcula a margem superior para centralizar verticalmente */
         }
 
         .project-list {
-            max-height: 300px; /* Altura máxima da lista para permitir a rolagem */
-            overflow-y: auto; /* Adicionando scroll apenas na direção vertical */
+            max-height: 300px;
+            /* Altura máxima da lista para permitir a rolagem */
+            overflow-y: auto;
+            /* Adicionando scroll apenas na direção vertical */
         }
 
         .project {
             display: flex;
             align-items: center;
-            justify-content: space-between; /* Alinha os itens ao longo do eixo principal */
+            justify-content: space-between;
+            /* Alinha os itens ao longo do eixo principal */
             margin-bottom: 20px;
         }
 
@@ -34,21 +44,25 @@
 
         .project-info {
             flex: 1;
-            max-width: calc(100% - 130px); /* Largura máxima menos a largura da imagem e da checkbox */
+            max-width: calc(100% - 130px);
+            /* Largura máxima menos a largura da imagem e da checkbox */
         }
 
         .description {
             overflow: hidden;
-            text-overflow: ellipsis; /* Adiciona reticências se o texto for muito longo */
+            text-overflow: ellipsis;
+            /* Adiciona reticências se o texto for muito longo */
         }
 
-        .checkbox {
-            margin-left: auto; /* Centraliza a checkbox à direita */
+        .checkbox,
+        .checkboxReceivers {
+            margin-left: auto;
+            /* Centraliza a checkbox à direita */
             margin-right: 10px;
         }
-        
     </style>
 </head>
+
 <body>
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#projectPopUpList">
         Ver Projetos
@@ -61,15 +75,16 @@
                 <div class="modal-header">
                     <h5 class="modal-title">Projetos</h5>
                     <div class="input-group" style="margin-left: auto; max-width: 300px;">
-                        <input type="text" class="form-control" placeholder="Pesquisar por nome ou descrição" id="searchInput">
+                        <input type="text" class="form-control" placeholder="Pesquisar por nome ou descrição"
+                            id="searchInput">
                     </div>
 
                 </div>
                 <div class="modal-body">
                     <div class="project-list">
 
-                    <!-- Script que carrega a lista de projetos -->
-                    <?php
+                        <!-- Script que carrega a lista de projetos -->
+                        <?php
                         // Incluir arquivo de ligação com a base de dados
                         require '../config/basedados.php';
 
@@ -86,9 +101,9 @@
                         $result = $conn->query($sql);
                         // Verifica se há resultados e exibe os projetos
                         if ($result->num_rows > 0) {
-                            while($row = $result->fetch_assoc()) {
+                            while ($row = $result->fetch_assoc()) {
                                 echo '<div class="project">';
-                                echo '<img src="../assets/projetos/' . $row["fotografia"] . '" class="project-image" alt="' . $row["nome"] . '" id="'.$row["fotografia"].'">';
+                                echo '<img src="../assets/projetos/' . $row["fotografia"] . '" class="project-image" alt="' . $row["nome"] . '" id="' . $row["fotografia"] . '">';
                                 echo '<div class="project-info">';
                                 echo '<h3>' . $row["nome"] . '</h3>';
                                 echo '<p>' . $row["descricao"] . '</p>';
@@ -100,11 +115,12 @@
                             echo "0 resultados";
                         }
 
-                    ?>
+                        ?>
                     </div>
                     <hr>
+                    <h5 class="modal-title">Destinatários</h5>
                     <div class="project-list">
-                    <?php
+                        <?php
                         $sql = "SELECT nome, fotografia, email FROM investigadores ORDER BY nome";
                         $result = $conn->query($sql);
                         // Verifica se há resultados e exibe os projetos
@@ -113,34 +129,35 @@
                         echo '<div class="project-info">';
                         echo '<h3>Newsletter</h3>';
                         echo '<p>Todos os emails subscritos à newsletter</p>';
-                        echo '<input type="checkbox" class="checkbox">';
+                        echo '<input type="checkbox" class="checkboxReceivers">';
                         echo '</div>';
                         echo '</div>';
 
                         if ($result->num_rows > 0) {
-                            while($row = $result->fetch_assoc()) {
+                            while ($row = $result->fetch_assoc()) {
                                 echo '<div class="project">';
                                 echo '<img src="../assets/investigadores/' . $row["fotografia"] . '" class="project-image" alt="' . $row["nome"] . '">';
                                 echo '<div class="project-info">';
                                 echo '<h3>' . $row["nome"] . '</h3>';
-                                echo '<p>' . $row["email"] . '</p>';
-                                echo '<input type="checkbox" class="checkbox">';
+                                echo '<p class="email">' . $row["email"] . '</p>';
+                                echo '<input type="checkbox" class="checkboxReceivers">';
                                 echo '</div>';
                                 echo '</div>';
                             }
                         } else {
                             echo "0 resultados";
                         }
-                        
+
                         // Fecha conexão
                         $conn->close();
-                    ?>
+                        ?>
                     </div>
 
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary" onclick="concluir()">Selecionar</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="limparSelecoes()">Fechar</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"
+                        onclick="limparSelecoes()">Fechar</button>
                 </div>
             </div>
         </div>
@@ -148,10 +165,10 @@
 
     <script>
         // Função para filtrar os projetos com base no texto da barra de pesquisa
-        document.getElementById('searchInput').addEventListener('input', function() {
+        document.getElementById('searchInput').addEventListener('input', function () {
             var searchText = this.value.toLowerCase();
             var projects = document.querySelectorAll('.project');
-            projects.forEach(function(project) {
+            projects.forEach(function (project) {
                 var projectName = project.querySelector('h3').textContent.toLowerCase();
                 var projectDescription = project.querySelector('p').textContent.toLowerCase();
                 if (projectName.includes(searchText) || projectDescription.includes(searchText)) {
@@ -161,47 +178,69 @@
                 }
             });
         });
-        
+
         function concluir() {
             var checkboxes = document.querySelectorAll('.checkbox');
-            var checkedItems = [];
+            var checkboxesReceivers = document.querySelectorAll('.checkboxReceivers');
+            var checkedProjects = [];
+            var checkedReceivers = [];
             var verificarCheckbox = false;
+            var verificarCheckboxR = false;
+            var sendToAll = false;
             // Obter os projetos selecionados
-            checkboxes.forEach(function(checkbox) {
+            checkboxes.forEach(function (checkbox) {
                 if (checkbox.checked) {
                     verificarCheckbox = true;
-                    var titulo = checkbox.parentElement.querySelector('h3').textContent;
-                    var desc = checkbox.parentElement.querySelector('p').textContent
-                    var imgid = checkbox.parentElement.parentElement.querySelector('img').id;
-                    var projeto = titulo + "||" + desc  + "||" + imgid;
-                    checkedItems.push(projeto);
+                    var projeto = {
+                        titulo: checkbox.parentElement.querySelector('h3').textContent,
+                        descricao: checkbox.parentElement.querySelector('p').textContent,
+                        imgid: checkbox.parentElement.parentElement.querySelector('img').id
+                    };
+                    checkedProjects.push(projeto);
                 }
             });
-            if (!verificarCheckbox) {
-                alert('Selecione pelo menos um projeto!');
-                return;
-            }else{
-                console.log(checkedItems);
 
+            checkboxesReceivers.forEach(function (checkbox) {
+                if (checkbox.checked) {
+                    verificarCheckboxR = true;
+                    email = checkbox.parentElement.querySelector('p').textContent;
+                    if (email.includes("Todos os")) {
+                        sendToAll = true;
+                    } else {
+                        
+                        var receiver = {
+                            email: email
+                        };
+                    }
+                    checkedReceivers.push(receiver);
+                }
+            });
+
+            if (!verificarCheckbox || !verificarCheckboxR) {
+                alert('Selecione pelo menos um projeto e um destinatário!');
+                return;
+            } else {
+                console.log(checkedProjects);
+                console.log(checkedReceivers);
                 // Enviar os dados para o PHP via AJAX
                 fetch('newsletterSender.php', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json; charset=utf-8',
                     },
-                    body: JSON.stringify(checkedItems),
+                    body: JSON.stringify({ checkedProjects: checkedProjects, checkedReceivers: checkedReceivers, sendToAll: sendToAll }),
                 })
-                .then(response => {
-                    if (response.ok) {
-                        //console.log('E-mail enviado com sucesso!');
-                        console.log(response.text());
-                    } else {
-                        console.error('Erro ao enviar e-mail:', response.statusText);
-                    }
-                })
-                .catch(error => {
-                    console.error('Erro ao enviar e-mail:', error);
-                });
+                    .then(response => {
+                        if (response.ok) {
+                            //console.log('E-mail enviado com sucesso!');
+                            return response.text();
+                        } else {
+                            console.error('Erro ao enviar e-mail:', response.statusText);
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Erro ao enviar e-mail:', error);
+                    });
 
                 // Fecha o modal simulando o clique no botão "Fechar"
                 document.querySelector('#projectPopUpList .modal-footer .btn-secondary').click();
@@ -209,7 +248,7 @@
                 input.value = '';
                 mostrarTodosProjetos();
             }
-            
+
         }
 
         function limparSelecoes() {
@@ -217,7 +256,7 @@
             var checkboxes = document.querySelectorAll('.checkbox');
             var input = document.getElementById('searchInput');
             input.value = '';
-            checkboxes.forEach(function(checkbox) {
+            checkboxes.forEach(function (checkbox) {
                 checkbox.checked = false;
             });
             mostrarTodosProjetos();
@@ -225,7 +264,7 @@
 
         function mostrarTodosProjetos() {
             var projects = document.querySelectorAll('.project');
-            projects.forEach(function(project) {
+            projects.forEach(function (project) {
                 project.style.display = 'flex';
             });
         }
@@ -236,4 +275,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- backoffice/newsletter/newsletterSender.php
+++ backoffice/newsletter/newsletterSender.php
@@ -1,119 +1,175 @@
 <html>
-  <head>
+
+<head>
   <meta charset="UTF-8">
-  </head>
+</head>
 <?php
 
 header('Content-Type: text/html; charset=utf-8');
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
 require 'lib/Exception.php';
 require 'lib/PHPMailer.php';
 require 'lib/SMTP.php';
 
 // Receber os dados do e-mail do JavaScript
-$dadosEmail = json_decode(file_get_contents('php://input'), true);
+$checkedItems = json_decode(file_get_contents('php://input'), true);
+$checkedProjectsJSON = $checkedItems['checkedProjects'];
+$checkedReceiversJSON = $checkedItems['checkedReceivers'];
+$checkedSend2AllJSON = $checkedItems['sendToAll'];
+echo json_encode($checkedProjectsJSON);
+echo json_encode($checkedReceiversJSON);
+echo json_encode($checkedSend2AllJSON);
 
-//echo("123456");
-
-
-$mail = new PHPMailer(); $mail->IsSMTP(); $mail->Mailer = "smtp";
-
-$mail->SMTPDebug  = 1;  
-$mail->SMTPAuth   = TRUE;
-$mail->SMTPSecure = "tls";
-$mail->Port       = 587;
-$mail->Host       = "smtp.gmail.com";
-$mail->Username   = "tiago0liveira.dev.test@gmail.com";
-$mail->Password   = "zpzr aewz cxda brjm";
-
-$mail->CharSet = "UTF-8";
-$mail->Encoding = "base64";
-
-$mail->IsHTML(true);
-$mail->AddAddress("tiago_oliveira2001@hotmail.com", "recipient-name");
-$mail->SetFrom("tiago0liveira.dev.test@gmail.com", "TechnArt");
-//$mail->AddReplyTo("iago_oliveira2001@hotmail.com", "reply-to-name");
-//$mail->AddCC("iago_oliveira2001@hotmail.com", "cc-recipient-name");
-$mail->addCustomHeader("MIME-Version", "1.0");
-$mail->addCustomHeader("Content-type", "text/html;charset=UTF-8");
-
-$mail->Subject = "TechnArt - Newsletter";
-
-
-$content = '';
-
-$content = '
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Email</title>
-      <style>
-      body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-      }
-      .x_container {
-        max-width: 90%;
-        padding: 8px;
-      }
-      .x_project-info {
-        margin-bottom: 20px;
-        display: flex;
-        align-items: center;
-      }
-      .x_project-text {
-        margin-left: 20px;
-      }
-      h2 {
-        color: #333;
-      }
-      p {
-        color: #666;
-      }
-      .x_project-image {
-        height: 90%;
-        width: auto;
-        max-height: 90%;
-      }
-      </style>
-      </head>
-      <body>
-      ';
-
-// Adiciona o corpo do e-mail ao conteúdo
-$content .= '<div class="container">';
-$content .= '<h1> TechnArt - Newsletter </h1>';
-
-
-foreach ($dadosEmail as $projeto) {
-  $array = explode("||", $projeto);
-  $content .= '<div style="margin-bottom: 20px; display: flex; align-items: center;">';
-  $content .= "<img src='http://novotechneart.ipt.pt/backoffice/assets/projetos/" .$array[2]. "' class='project-image' alt='ImagemProjeto' width='350' height='250'>";
-  $content .= '<div style="margin-left: 20px;">';
-  $content .= "<p><b>" . $array[0] . "</b></p>";
-  $content .= "<p>" . $array[1] . "</p>";
-  $content .= "</div>";
-  $content .= "</div>";
-}
-$content .= "</div>";
-$content .= '</body>';
-$content .= '</html>';
-
-
-
- 
-$mail->MsgHTML($content); 
-if(!$mail->Send()) {
-  echo "Error while sending Email.";
-  var_dump($mail);
+// Verificar se os dados foram recebidos com sucesso
+if ($checkedItems === null) {
+  // Se os dados não puderem ser decodificados, houve um erro
+  $response = ['error' => 'Erro ao receber os dados.'];
 } else {
-  echo "Email sent successfully";
+  // Os dados foram recebidos com sucesso
+  // Agora podemos fazer o que quisermos com os dados
+  // Neste exemplo, apenas retornaremos os dados recebidos
+  $mail = new PHPMailer();
+  $mail->IsSMTP();
+  $mail->Mailer = "smtp";
+
+  $mail->SMTPDebug = 1;
+  $mail->SMTPAuth = TRUE;
+  $mail->SMTPSecure = "tls";
+  $mail->Port = 587;
+  $mail->Host = "smtp.gmail.com";
+  $mail->Username = "tiago0liveira.dev.test@gmail.com";
+  $mail->Password = "zpzr aewz cxda brjm";
+
+  $mail->CharSet = "UTF-8";
+  $mail->Encoding = "base64";
+
+  $mail->IsHTML(true);
+  $mail->ContentType = 'text/html; charset=utf-8';
+  //$mail->AddAddress("tiago_oliveira2001@hotmail.com", "recipient-name");
+  $mail->SetFrom("tiago0liveira.dev.test@gmail.com", "TechnArt");
+  //$mail->AddReplyTo("iago_oliveira2001@hotmail.com", "reply-to-name");
+  //$mail->AddCC("iago_oliveira2001@hotmail.com", "cc-recipient-name");
+
+  $mail->Subject = "TechnArt - Newsletter";
+
+
+  $content = '';
+
+  $content = '
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Email</title>
+        <style>
+        body {
+          font-family: Arial, sans-serif;
+          margin: 0;
+          padding: 0;
+        }
+        .x_container {
+          max-width: 90%;
+          padding: 8px;
+        }
+        .x_project-info {
+          margin-bottom: 20px;
+          display: flex;
+          align-items: center;
+        }
+        .x_project-text {
+          margin-left: 20px;
+        }
+        h2 {
+          color: #333;
+        }
+        p {
+          color: #666;
+        }
+        .x_project-image {
+          height: 90%;
+          width: auto;
+          max-height: 90%;
+        }
+        </style>
+        </head>
+        <body>
+        ';
+
+  // Adiciona o corpo do e-mail ao conteúdo
+  $content .= '<div class="container">';
+  $content .= '<h1> TechnArt - Newsletter </h1>';
+
+  foreach ($checkedProjectsJSON as $project) {
+    $titulo = $project['titulo'];
+    $desc = $project['descricao'];
+    $img = $project['imgid'];
+
+    //echo "Título: $titulo, Descrição: $desc, Imagem: $img<br>";
+
+    $content .= '<div style="margin-bottom: 20px; display: flex; align-items: center;">';
+    $content .= "<img src='http://novotechneart.ipt.pt/backoffice/assets/projetos/" . $img . "' class='project-image' alt='ImagemProjeto' width='350' height='250'>";
+    $content .= '<div style="margin-left: 20px;">';
+    $content .= "<p><b>" . $titulo . "</b></p>";
+    $content .= "<p>" . $desc . "</p>";
+    $content .= "</div>";
+    $content .= "</div>";
+
+
+  }
+
+  $content .= "</div>";
+  $content .= '</body>';
+  $content .= '</html>';
+
+
+  foreach ($checkedReceiversJSON as $email) {
+    $mail->AddAddress($email["email"]);
+  }
+
+  if ($checkedSend2AllJSON) {
+    // Incluir arquivo de ligação com a base de dados
+    require '../config/basedados.php';
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+    mysqli_set_charset($conn, $charset);
+
+    $sql = "SELECT email FROM newsletter";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+
+        $mail->AddAddress($row["email"]);
+      }
+    } else {
+      echo "0 resultados";
+    }
+
+  }
+
+  $mail->MsgHTML($content);
+
+  if (!$mail->Send()) {
+    echo "Error while sending Email.";
+    var_dump($mail);
+  } else {
+    echo "Email sent successfully";
+  }
+  $response = ['message' => 'Dados recebidos com sucesso.', 'data' => $checkedItems];
 }
+
+
+
 
 http_response_code(200);
 exit;
