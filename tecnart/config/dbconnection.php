0c0f97af432684b58da0e3356f2ce848df995686
pzmq <rs_monge@hotmail.com>
2024-05-08 16:11:00 +0100

    Revert "Merge branch 'master' into 129-story-29---task-7---homepage---modifyshorten-the-footer"
    This reverts commit bcb3633cd172cab166093a17a561a2219af54244, reversing
    changes made to 845c3c5536385a47d829f4d44bf42e3fe126de4f.

--- tecnart/config/dbconnection.php
+++ tecnart/config/dbconnection.php
@@ -2,15 +2,10 @@
 require 'credentials.php';
 
 function pdo_connect_mysql() {
-    // $host = '127.0.0.1';
-    // $db = 'technart';
-    // $user = USERNAME;
-    // $pass = PASSWORD;
-    $host = "94.46.180.24";
-    $user = 'costa';
-    $pass = '12345';
-
-    $db = "Tech-Art";
+    $host = '127.0.0.1';
+    $db = 'technart';
+    $user = USERNAME;
+    $pass = PASSWORD;
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
